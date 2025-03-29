93408e082d3811aad4b0b3a4af114cbd610bfdab
Bryan Shepherd <thespamhouse@gmail.com>
2025-03-29 02:25:00 +0000

    rewrite to automatically backup data

--- fetch_data.py
+++ fetch_data.py
@@ -8,8 +8,11 @@ import pandas as pd
 
 url = 'https://api.5calls.org/v1/issues'
 
-def parse_json_to_db(data, utc_time, db_name="data/fivecalls.db", table_name="flat_data"):
+backup_path = '/home/Projects/fivecallsdatabackup/'
+
+def parse_json_to_db(data, utc_time, db_name="fivecalls.db", table_name="flat_data", backup_path=backup_path):
     os.makedirs("data", exist_ok=True)
+    os.makedirs(backup_path, exist_ok=True)
     df = pd.json_normalize(data)
     df['inserted_at_utc'] = utc_time
     df['categories'] = df['categories'].apply(json.dumps)
@@ -17,19 +20,24 @@ def parse_json_to_db(data, utc_time, db_name="data/fivecalls.db", table_name="fl
     df['outcomeModels'] = df['outcomeModels'].apply(json.dumps)
     if 'stats.calls' in df.columns:
         df.rename(columns={'stats.calls': 'calls'}, inplace=True)
-    with sqlite3.connect(db_name) as conn:
-        df.to_sql(table_name, conn, if_exists='append', index=False)
-    print(f"✅ Appended {len(df)} records to {table_name} in {db_name}.")
+    # Write to working directory
+    for path in ('data/', backup_path):
+        db_name_tmp = os.path.join(path, db_name)
+        print(f"Writing to {db_name_tmp}...")
+        with sqlite3.connect(db_name_tmp) as conn:
+            df.to_sql(table_name, conn, if_exists='append', index=False)
+        print(f"✅ Appended {len(df)} records to {table_name} in {db_name_tmp}.")
 
-def fetch_and_store(keep_original_data=False):
+def fetch_and_store(keep_original_data=True, backup_path=backup_path):
     response = requests.get(url)
     utc_time = datetime.now(timezone.utc).isoformat()
     if response.status_code == 200:
         data = response.json()
         parse_json_to_db(data, utc_time)
         if keep_original_data:
-            os.makedirs("original_data", exist_ok=True)
-            with open(f'original_data/data_{utc_time}.json', 'w') as f:
+            backup_path = os.path.join(backup_path, 'original_data')
+            os.makedirs(backup_path, exist_ok=True)
+            with open(f'{backup_path}/data_{utc_time}.json', 'w') as f:
                 json.dump(data, f)
             print(f"✅ Data saved at {utc_time}")
     else:
